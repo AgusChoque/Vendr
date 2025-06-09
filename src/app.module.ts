7e2bb3b171ec42599bec2a29e75b706782d634c6
agustinchoque <agustinchoque.dev@gmail.com>
2025-06-09 16:30:43 -0300

    chore: initial setup for development and models definition

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { BusinessModule } from './modules/business/business.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    BusinessModule,
+    CategoriesModule,
+    ProductsModule,
+    OrdersModule,
+  ],
   controllers: [],
   providers: [],
 })
